88cd224b3fa745d771e0386d7d1fbec73ae87445
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-06-12 12:09:03 +0900

    Time: 91 ms (51.31%), Space: 43.4 MB (96.33%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -4,19 +4,23 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    if (ransomNote.length > magazine.length) {
-        return false;
-    }
+    const map = new Map();
 
-    let magazineCopy = magazine;
-
-    for (const character of ransomNote) {
-        if (magazineCopy.indexOf(character) !== -1) {
-            magazineCopy = magazineCopy.replace(character, "");
+    for (let i = 0; i < magazine.length; i++) {
+        if (map.has(magazine[i])) {
+            map.set(magazine[i], map.get(magazine[i]) + 1)
             continue;
         }
-    
-        return false;
+
+        map.set(magazine[i], 1)
+    }
+
+    for (let i = 0; i < ransomNote.length; i++) {
+        if (!map.has(ransomNote[i]) || map.get(ransomNote[i]) === 0) {
+            return false;
+        }
+
+        map.set(ransomNote[i], map.get(ransomNote[i]) - 1)
     }
 
     return true;

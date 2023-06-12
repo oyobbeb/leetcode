a7eceae7986fdb3918a923f2762f14ad52f7a3a3
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-06-12 12:02:06 +0900

    Time: 97 ms (34.21%), Space: 45.2 MB (57.12%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -4,37 +4,12 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-    const subset = {};
-    const superset = {};
+    let dupRansomNote = ransomNote;
 
-    for (let i = 0; i < ransomNote.length; i++) {
-        const stringValue = ransomNote[i];
-
-        if (subset[stringValue]) {
-            subset[stringValue]++;
-            continue;
-        }
-
-        subset[stringValue] = 1;
+    for (const char of magazine) {
+        dupRansomNote = dupRansomNote.replace(char, "");
     }
 
-    for (let i = 0; i < magazine.length; i++) {
-        const stringValue = magazine[i];
-
-        if (superset[stringValue]) {
-            superset[stringValue]++;
-            continue;
-        }
-
-        superset[stringValue] = 1;
-    }
-  
-    console.log(subset, superset);
-
-    for (const x in subset) {
-        if (!superset[x]) return false;
-        if (superset[x] && subset[x] > superset[x]) return false;
-    }
-
-    return true;
+    if (!dupRansomNote) return true;
+    return false;
 };

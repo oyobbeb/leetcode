7da587ab1a26f3854db911b36ab2c1e20d2e43b8
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-06-13 14:22:22 +0900

    Time: 75 ms (18.39%), Space: 43.9 MB (69.18%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -3,36 +3,20 @@
  * @return {number}
  */
 var longestPalindrome = function(s) {
-    if (s.length < 3 && s[0] !== s[1]) return 1;
-    if (s.length < 3) return s.length; 
-
-    const obj = {};
     let count = 0;
-    let isOddNumber = false;
-
+    const set = new Set();
+    
     for (let i = 0; i < s.length; i++) {
         const string = s[i];
 
-        if (obj[string]) {
-            obj[string]++;
+        if (set.has(string)) {
+            count += 2;
+            set.delete(string);
             continue;
         }
 
-        obj[string] = 1;
+        set.add(string);
     }
 
-    for (const key in obj) {
-        if (obj[key] % 2 !== 0) {
-            count += obj[key] - 1;
-            isOddNumber = true;
-        }
-
-        if (obj[key] % 2 === 0) {
-            count += obj[key];
-        }
-    }
-
-    console.log(count);
-
-    return isOddNumber ? count + 1 : count;
+    return count + (set.size > 0 ? 1 : 0);
 };

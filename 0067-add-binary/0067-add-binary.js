bca74d1412257ff8e58e34caf25f07e95a23fb26
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-06-14 14:09:31 +0900

    Time: 82 ms (7.8%), Space: 44 MB (48.12%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -8,36 +8,23 @@ var addBinary = function(a, b) {
     let carry = 0;
     let index = 0;
 
-    while (index < Math.max(a.length, b.length)) {
-        let x = 0;
-        let y = 0;
+    while (a[index] || b[index]) {
+        const sum = Number(a[a.length - index - 1] || 0) + Number(b[b.length - 1 - index] || 0) + carry;
 
-        if (a.length - 1- index >= 0) {
-            x = parseInt(a[a.length - 1 - index], 2);
+        if (sum > 1) {
+            result = `${sum === 2 ? "0" : "1"}${result}`;
+            carry = 1;
         }
 
-        if (b.length - 1 - index >= 0) {
-            y = parseInt(b[b.length - 1 - index], 2);
-        }
-
-        let sum = carry + x + y;
 
         if (sum <= 1) {
             result = `${sum}${result}`;
             carry = 0;
         }
 
-        if (sum > 1) {
-            result = `${sum % 2}${result}`;
-            carry = 1;
-        }
 
         index++;
     }
 
-    if (carry === 1) {
-        result = `${carry}${result}`
-    }
-
-    return result;
+    return `${carry || ""}${result}`;
 };

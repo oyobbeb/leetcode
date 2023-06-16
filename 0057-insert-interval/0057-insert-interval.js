41234e644082a8a2b9229a670d08f4e1eec43df8
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-06-16 15:46:07 +0900

    Time: 76 ms (43.42%), Space: 44.5 MB (49.07%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+    const left = [];
+    const right = [];
+    let [start, end] = newInterval;
+
+    for (const interval of intervals) {
+        const [first, last] = interval;
+
+        if (last < start) {
+            left.push(interval);
+            continue;
+        };
+
+        if (first > end) {
+            right.push(interval);
+            continue;
+        };
+
+        start = start > first ? first : start;
+        end = end > last ? end : last;
+    }
+
+    return [...left, [start, end], ...right];
+};

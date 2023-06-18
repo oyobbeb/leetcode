17406cfa14a85b7db3e7487a6ac99c5aefb4e831
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-06-18 19:37:00 +0900

    Time: 46 ms (98.06%), Space: 41.3 MB (97.42%) - LeetHub

--- 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.js
+++ 1769-get-maximum-in-generated-array/1769-get-maximum-in-generated-array.js
@@ -2,20 +2,19 @@
  * @param {number} n
  * @return {number}
  */
+
 var getMaximumGenerated = function(n) {
   if (n === 0) return 0;
-  if (n <= 2) return 1;
-
+  if (n === 1) return 1;
   const nums = [0, 1];
 
-  for (let i = 1; i < n; i++) {
-    nums[i * 2] = nums[i];
+  for (let i = 2; i <= n; i++) {
+    if (i % 2 === 0) {
+      nums[i] = nums[i / 2];
+      continue;
+    }
 
-    if (i * 2 === n) break;
-
-    nums[(i * 2) + 1] = nums[i] + nums[i + 1];
-
-    if ((i * 2) + 1 === n) break;
+    nums[i] = nums[(i - 1) / 2] + nums[((i - 1) / 2) + 1];
   }
 
   return Math.max(...nums);

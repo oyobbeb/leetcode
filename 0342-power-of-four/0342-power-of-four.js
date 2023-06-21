d3cf59e61a5e5ca82fea07767d85073e38896ff5
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-06-21 20:16:23 +0900

    Time: 78 ms (18.34%), Space: 43 MB (92.87%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if (n < 1) return false;
+    if (n === 1) return true;
+
+    return isPowerOfFour(n / 4);
+};

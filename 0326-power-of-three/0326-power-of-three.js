c81ad5d9733ac20616eb75cfc1927b6623c68bf3
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-06-21 20:17:14 +0900

    Time: 218 ms (54.61%), Space: 51 MB (75.44%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.js
+++ 0326-power-of-three/0326-power-of-three.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    if (n === 0) return false;
+    if (n < 1) return false;
     if (n === 1) return true;
 
     return isPowerOfThree(n / 3);

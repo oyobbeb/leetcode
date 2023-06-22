92823cc37e0e244eba6ebdc1876c7edd5690279b
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-06-22 12:27:25 +0900

    Time: 75 ms (14.63%), Space: 44.4 MB (28.57%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
@@ -3,15 +3,11 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-  const memo = new Array(cost.length + 3);
+  const memo = new Array(cost.length + 1).fill(0);
 
-  memo[cost.length] = 0;
-  memo[cost.length + 1] = 0;
-  memo[cost.length + 2] = 0;
-
-  for (let i = cost.length - 1; i >= 0; i--) {
-    memo[i] = cost[i] + Math.min(memo[i + 1], memo[i + 2]);
+  for (let i = 3; i < cost.length + 3; i++) {
+    memo[i] = cost[i - 3] + Math.min(memo[i - 1], memo[i - 2]);
   }
 
-  return Math.min(memo[0], memo[1]);
+  return Math.min(memo[memo.length - 1], memo[memo.length - 2]);
 };

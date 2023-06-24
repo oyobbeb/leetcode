22c511d544f27cb80b13926c6e9d506e4648124b
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-06-24 16:09:16 +0900

    Time: 85 ms (43.96%), Space: 50.6 MB (20.49%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,6 +3,9 @@
  * @return {number}
  */
 var maxSubArray = function(array) {
+  // time complexyity: O(n)
+  // space complexity: O(1)
+
   for (let i = 1; i < array.length; i++) {
     array[i] = Math.max(array[i], array[i] + array[i - 1]);
   }

1da712c3fd61e366fad1b49df50e09794d57eb16
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-07-12 20:15:03 +0900

    Time: 130 ms (20.91%), Space: 47.7 MB (38.31%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+  // Time Complexity: O(n log n);
+  // Space Complextiy: O(1);
+
+  let totalSum = 0;
+
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i += 2) {
+    totalSum += nums[i];
+  }
+
+  return totalSum;
+};

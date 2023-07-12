57218d8f1ff752a67068711b61c42a652680e6c5
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-07-12 21:01:08 +0900

    Time: 75 ms (53.38%), Space: 44.8 MB (37.15%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,6 +3,21 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+  // Time Complexity: O(N^2);
+  // Space Complexity: O(1);
+
+  // if (nums.length === 0) return 0;
+
+  // for(let i = 0; i < nums.length; i++) {
+  //   if (nums[i] === nums[i + 1]) {
+  //     nums.splice(i, 1);
+  //     i--;
+  //   }
+  // }
+
+  // return nums.length;
+
+
   // Time Complexity: O(N);
   // Space Complexity: O(1);
 

c11c4c3336c889924d671893693cb161f739f485
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-07-13 23:54:54 +0900

    Time: 67 ms (37.06%), Space: 42.2 MB (63%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  // Time Complexity: O(MN)
+  // Space Complexity: O(1)
+
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] !== strs[0][i]) {
+        return strs[0].substring(0, i);
+      }
+    }
+  }
+
+  return strs[0];
+};

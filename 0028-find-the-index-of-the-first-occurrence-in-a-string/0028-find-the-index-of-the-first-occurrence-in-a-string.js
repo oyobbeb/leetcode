99060a84afd81fa1155d4c3c33807486c3a525d6
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-07-15 16:37:58 +0900

    Time: 64 ms (29.00%), Space: 41.6 MB (86.15%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,5 +4,25 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-  return haystack.indexOf(needle);
+  /**
+  Time Complexity: O(NM)
+  Space Complexity: O(1)
+  */
+
+  let j = 0;
+
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === needle[j]) {
+      j++;
+      if (j === needle.length) {
+        return i - (j - 1);
+      }
+      continue;
+    }
+
+    i -= j;
+    j = 0;
+  }
+
+  return -1;
 };

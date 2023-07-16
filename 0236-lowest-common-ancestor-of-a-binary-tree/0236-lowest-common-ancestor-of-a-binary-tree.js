148caa12c56c32741d6e01046a28e503c691503e
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-07-16 23:06:46 +0900

    Time: 86 ms (35.81%), Space: 51.4 MB (91.02%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+  /**
+  * Time Complexity: O(N)
+  * Space Complexity: O(logN) or O(N)
+  */
+  
+  if (!root || root === p || root === q) return root;
+
+  const left = lowestCommonAncestor(root.left, p, q);
+  const right = lowestCommonAncestor(root.right, p, q);
+  
+  if (left && right) {
+    return root;
+  }
+
+  return left ? left : right;
+};

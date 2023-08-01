b9266696fe1213416aaa4cd9f2e1ce5471fb63fb
oyobbeb <113571767+oyobbeb@users.noreply.github.com>
2023-08-01 16:32:08 +0900

    Time: 107 ms (19.13%), Space: 44 MB (35.59%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+  for (let i = 0; i <= x; i++) {
+    if (i * i === x) return i;
+    if (i * i < x && (i + 1) * (i + 1) > x) return i;
+  }
+};

0f317f53942776c5e05e8179ae408601dcdaf09f
Yeongbin Jeong <113571767+oyobbeb@users.noreply.github.com>
2023-10-22 14:05:43 +0900

    Time: 61 ms (58.21%), Space: 48.7 MB (11.19%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,28 +3,28 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-  // const map = new Map();
+  const map = new Map();
 
-  // for (const num of nums) {
-  //   if (map.has(num)) {
-  //     map.set(num, map.get(num) + 1);
-  //     continue;
-  //   }
+  for (const num of nums) {
+    if (map.has(num)) {
+      map.set(num, map.get(num) + 1);
+      continue;
+    }
 
-  //   map.set(num, 1);
-  // }
+    map.set(num, 1);
+  }
 
-  // for (const [key, value] of map) {
-  //   if (value === 1) return key;
-  // }
+  for (const [key, value] of map) {
+    if (value === 1) return key;
+  }
 
   // return null;
 
-  nums.sort();
+  // nums.sort();
 
-  for (let i = 0; i < nums.length - 1; i += 2) {
-    if (nums[i] !== nums[i + 1]) return nums[i];
-  }
+  // for (let i = 0; i < nums.length - 1; i += 2) {
+  //   if (nums[i] !== nums[i + 1]) return nums[i];
+  // }
 
-  return nums[nums.length - 1]
+  // return nums[nums.length - 1];
 };
